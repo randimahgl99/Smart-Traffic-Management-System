e93d1ed254b1ed016b73c74a9289c351729d5e97
Neshadi Ranaweera <117301828+Neshadi@users.noreply.github.com>
2026-05-26 09:02:39 +0530

    update traffic_hybrid

--- traffic_hybrid_mpi_openmp.c
+++ traffic_hybrid_mpi_openmp.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    srand(42 + rank); // FIXED SEED CONSISTENCY
+    srand(42 + rank); 
 
     if(rank == 0)
     {
